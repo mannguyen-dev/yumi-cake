a494255087e30450ff95b06b4927125b7b55f544
hoangvu0397 <thv0897@gmail.com>
2022-10-15 03:19:26 +0700

    slider_done

--- frontend/yumicakefe/src/Pages/Home/Home.js
+++ frontend/yumicakefe/src/Pages/Home/Home.js
@@ -1,13 +1,17 @@
 import { Fragment } from "react";
 import PopularCakes from "../../components/cake/PopularCakes";
+import SliderCake from "../../components/cake/SliderCake";
 
 const Home = () => {
-    return (
-        <Fragment>
-            <PopularCakes title="Phổ biến..." />
-            <PopularCakes title="Đang thịnh hành..." />
-        </Fragment>
-    );
+  return (
+    <div>
+      <Fragment>
+        <SliderCake />
+        <PopularCakes title="Phổ biến..." />
+        <PopularCakes title="Đang thịnh hành..." />
+      </Fragment>
+    </div>
+  );
 };
 
 export default Home;
