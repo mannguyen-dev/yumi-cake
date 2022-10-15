314fdab36a3add97b762be79f33acf86d728f84b
hoangvu0397 <thv0897@gmail.com>
2022-10-15 14:24:06 +0700

    aboutus_done

--- frontend/yumicakefe/src/App.js
+++ frontend/yumicakefe/src/App.js
@@ -5,46 +5,46 @@ import { Route, Switch, Redirect } from "react-router-dom";
 import NotFound from "./Pages/NotFound";
 import Layout from "./components/Layout/Layout";
 import ShowAllCakes from "./components/cake/ShowAllCake";
-
+import AboutUs from "./components/about/AboutUs";
 function App() {
-    return (
-        <Layout>
-            <div className="App">
-                <Switch>
-                    <Route path="/" exact>
-                        <Redirect to="/home" />
-                    </Route>
-                    <Route path="/home" exact>
-                        <Home />
-                    </Route>
-                    <Route path="/products" exact>
-                        <ShowAllCakes />
-                    </Route>
-                    <Route path="/products/cat/:category">
-                        <Home />
-                    </Route>
-                    <Route path="/products/id/:quoteId">
-                        <Home />
-                    </Route>
-                    <Route path="/user">
-                        <Home />
-                    </Route>
-                    <Route path="/about">
-                        <Home />
-                    </Route>
-                    <Route path="/login">
-                        <Home />
-                    </Route>
-                    <Route path="/signup">
-                        <Home />
-                    </Route>
-                    <Route path="*">
-                        <NotFound />
-                    </Route>
-                </Switch>
-            </div>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/home" />
+          </Route>
+          <Route path="/home" exact>
+            <Home />
+          </Route>
+          <Route path="/products" exact>
+            <ShowAllCakes />
+          </Route>
+          <Route path="/products/cat/:category">
+            <Home />
+          </Route>
+          <Route path="/products/id/:quoteId">
+            <Home />
+          </Route>
+          <Route path="/user">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <AboutUs />
+          </Route>
+          <Route path="/login">
+            <Home />
+          </Route>
+          <Route path="/signup">
+            <Home />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </div>
+    </Layout>
+  );
 }
 
 export default App;
