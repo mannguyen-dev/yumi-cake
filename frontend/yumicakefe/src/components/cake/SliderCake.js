45788e4245f9fe8dc11d2b0994626ec959376a2f
MANNGOC.NGUYEN <mannguyensp@gmail.com>
2022-10-16 23:27:23 +0700

    Man #16: Optimize slider

--- frontend/yumicakefe/src/components/cake/SliderCake.js
+++ frontend/yumicakefe/src/components/cake/SliderCake.js
@@ -1,44 +1,32 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 import { Link } from "react-router-dom";
+import slideImg1 from "../../assets/slider/Slide_1.jpg";
+import slideImg2 from "../../assets/slider/Slide_2.jpg";
+import slideImg3 from "../../assets/slider/Slide_3.jpg";
+import slideImg4 from "../../assets/slider/Slide_4.jpg";
 
 function SliderCake() {
     return (
         <Carousel>
             <Carousel.Item interval={1500}>
                 <Link to="/products">
-                    <img
-                        className="d-block w-100"
-                        src={"https://media.bakingo.com/60_min_delivery_bk_hp_banner_d_n.jpg"}
-                        alt="First slide"
-                    />
+                    <img className="d-block w-100" src={slideImg1} alt="First slide" />
                 </Link>
             </Carousel.Item>
             <Carousel.Item interval={1000}>
                 <Link to="/products">
-                    <img
-                        className="d-block w-100"
-                        src="https://media.bakingo.com/kids_cake_bk_hp_banner_d.jpg"
-                        alt="Second slide"
-                    />
+                    <img className="d-block w-100" src={slideImg2} alt="Second slide" />
                 </Link>
             </Carousel.Item>
             <Carousel.Item interval={500}>
                 <Link to="/products">
-                    <img
-                        className="d-block w-100"
-                        src="https://media.bakingo.com/boss_day_bk_hp_banner_d.jpg"
-                        alt="Third slide"
-                    />
+                    <img className="d-block w-100" src={slideImg3} alt="Third slide" />
                 </Link>
             </Carousel.Item>
             <Carousel.Item>
                 <Link to="/products">
-                    <img
-                        className="d-block w-100"
-                        src="https://media.bakingo.com/heavenly_cake_bk_hp_banner_d_0.jpg"
-                        alt="Third slide"
-                    />
+                    <img className="d-block w-100" src={slideImg4} alt="Third slide" />
                 </Link>
             </Carousel.Item>
         </Carousel>
